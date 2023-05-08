441c50deccca9ecb9e5568a92ca937fa5f192651
Manika <manika.aditya@gmail.com>
2023-05-08 15:32:07 +0530

    added basic functionalities

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ console.log("Welcome to Spotify");
 
 // Initialize the variables
 let songIndex = 0;
-let audioElement = new Audio('./songs/Raatan Lambiyan(PagalWorld.com.se).mp3');
+let audioElement = new Audio('./songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Night Changes", filePath: "songs/Night-Changes(PagalWorld).mp3", coverPath: "covers/Night Changes.jpeg"},
-    {songName: "Heat Waves", filePath: "songs/Heat-Waves(PagalWorld).mp3", coverPath: "covers/heat-waves.webp"},
-    {songName: "Dandelions", filePath: "songs/Dandelions(PagalWorld).mp3", coverPath: "covers/Dandelions.jpeg"},
-    {songName: "Peaches", filePath: "songs/_Peaches(PagalWorld.com.se).mp3", coverPath: "covers/Peaches.jpg"},
-    {songName: "We Rollin", filePath: "songs/We Rollin - Shubh.mp3", coverPath: "covers/We-rollin.jpg"},
-    {songName: "Perfect", filePath: "songs/Perfect.mp3", coverPath: "covers/Perfect-Lyrics-150x150.webp"},
-    {songName: "Raatan Lambiyan", filePath: "songs/Raatan Lambiyan(PagalWorld.com.se).mp3", coverPath: "covers/Raataan Lambiyan.jpeg"},
-    {songName: "Victory(Instrumental) Lakshya", filePath: "songs/128-Victory (Instrumental) - Lakshya 128 Kbps.mp3", coverPath: "covers/Victory (Instrumental) - Lakshya.jpg"},
-    {songName: "There's nothing holding me back", filePath: "songs/Theres_Nothing_Holding_Me_Back.mp3", coverPath: "covers/There's nothing holdin' me back.jpg"}
+    {songName: "Night Changes", filePath: "songs/1.mp3", coverPath: "covers/Night Changes.jpeg"},
+    {songName: "Heat Waves", filePath: "songs/2.mp3", coverPath: "covers/heat-waves.webp"},
+    {songName: "Dandelions", filePath: "songs/3.mp3", coverPath: "covers/Dandelions.jpeg"},
+    {songName: "Peaches", filePath: "songs/4.mp3", coverPath: "covers/Peaches.jpg"},
+    {songName: "We Rollin", filePath: "songs/5.mp3", coverPath: "covers/We-rollin.jpg"},
+    {songName: "Perfect", filePath: "songs/6.mp3", coverPath: "covers/Perfect-Lyrics-150x150.webp"},
+    {songName: "Raatan Lambiyan", filePath: "songs/7.mp3", coverPath: "covers/Raataan Lambiyan.jpeg"},
+    {songName: "Victory(Instrumental) Lakshya", filePath: "songs/8.mp3", coverPath: "covers/Victory (Instrumental) - Lakshya.jpg"},
+    {songName: "There's nothing holding me back", filePath: "songs/9.mp3", coverPath: "covers/There's nothing holdin' me back.jpg"}
 ]
 
 songItems.forEach((element, i )=>{
-    console.log(element, i)
     element.getElementsByTagName('img')[0].src = songs[i].coverPath;
     element.getElementsByClassName('songName')[0].innerText = songs[i].songName;
 })
@@ -55,3 +55,54 @@ myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime = myProgressBar.value*audioElement.duration/100;
 })
 
+const makeAllPlays = () =>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `songs/${songIndex+1}.mp3`
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterSongName.innerText = songs[songIndex].songName;
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=8){
+        songIndex = 0;
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+})
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0;
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+})
